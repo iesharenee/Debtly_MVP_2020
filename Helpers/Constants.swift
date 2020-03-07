b11541e0250277b5af41f5e7ee581b22804e5951
Iesha Magallanes <imagalla@nd.edu>
2020-03-07 14:30:48 -0500

    Debtly Update

---
+++ Helpers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Debtly-App
+//
+//  Created by Iesha Magallanes on 2/19/20.
+//  Copyright © 2020 Debtly. All rights reserved.
+//
+
+import Foundation
+struct Constants {
+    
+    struct Storyboard {
+        
+       static let homeViewController = "HomeVC"
+       static let accountsViewController = "AccountsVC"
+    }
+    
+   
+}
